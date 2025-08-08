5cfdb0c312f70ef56a8d36aeb508cb7d035a9a35
Chinmay <dipkechinmay@gmail.com>
2025-08-08 11:25:39 +0530

    rotate 90 deg

--- ATM/src/DSA/Rotate_90.java
+++ ATM/src/DSA/Rotate_90.java
@@ -3,8 +3,66 @@ package DSA;
 public class Rotate_90 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		int[][] arr = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9},
+		};
+		
+		rotate(arr);
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[0].length; j++) {
+				
+				System.out.print(arr[i][j]+" ");
+				
+				
+			}
+			System.out.println();
+		}
+		
 	}
 
+	private static void rotate(int[][] arr) {
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 1; j < arr[0].length; j++) {
+				
+				int temp = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+				
+			}
+		}
+		
+		System.out.println("=====Transpose======");
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[0].length; j++) {
+				
+				System.out.print(arr[i][j]+" ");
+				
+				
+			}
+			System.out.println();
+		}
+		System.out.println("===========");
+		
+		for(int i = 0; i < arr.length; i++) {
+			int start = 0;
+			int end = arr[0].length - 1;
+
+			for(int j = 1; j < arr[0].length; j++) {				
+				int temp = arr[i][start];
+				arr[i][start] = arr[i][end];
+				arr[i][end] = temp;
+				start++;
+				end--;
+				
+			}
+		}
+		
+	}
+	
+	
 }
